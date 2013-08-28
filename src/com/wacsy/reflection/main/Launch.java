8669bdc4c97ce93a3a50a8734bcec70641550d83
soy502636321 <soy502636321@gmail.com>
2013-08-29 00:44:56 +0800

    反射第一课

--- src/com/wacsy/reflection/main/Launch.java
+++ src/com/wacsy/reflection/main/Launch.java
@@ -1,28 +1,48 @@
 package com.wacsy.reflection.main;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Vector;
 
 import com.wacsy.reflection.Circle;
+import com.wacsy.reflection.Cookie;
 import com.wacsy.reflection.Shape;
 import com.wacsy.reflection.Square;
 import com.wacsy.reflection.Triangle;
 
 public class Launch {
 	public static void main(String[] args) {
-		Vector<Shape> shapes = new Vector<Shape>();
+//		Vector<Shape> shapes = new Vector<Shape>();
+//		
+//		//增加形状元素
+//		shapes.add(new Circle());
+//		shapes.add(new Square());
+//		shapes.add(new Triangle());
+//		
+//		//
+//		Enumeration<Shape> enumeration = shapes.elements();
+//		
+//		while (enumeration.hasMoreElements()) {
+//			Shape shape = (Shape) enumeration.nextElement();
+//			shape.draw();
+//		}
 		
-		//增加形状元素
-		shapes.add(new Circle());
-		shapes.add(new Square());
-		shapes.add(new Triangle());
+		Class<Cookie> clazz = Cookie.class;
+		Constructor<?>[] constructors = clazz.getConstructors();
+		Method[] methods = clazz.getMethods();
 		
-		//
-		Enumeration<Shape> enumeration = shapes.elements();
+		for (Constructor<?> constructor : constructors) {
+//			Object object = constructor;
+			Constructor<Cookie> constructor_Cookie = (Constructor) constructor;
+			System.out.println(constructor_Cookie);
+		}
 		
-		while (enumeration.hasMoreElements()) {
-			Shape shape = (Shape) enumeration.nextElement();
-			shape.draw();
+		for (Method method : methods) {
+			System.out.println(method.toGenericString());
 		}
 	}
 }
